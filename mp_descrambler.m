404578709055bc42c8d3d6bbc7c8c25dcd25bd02
PjLyoko <pjlyoko@gmail.com>
2018-05-07 21:55:33 +0200

    Repaired function

--- mp_descrambler.m
+++ mp_descrambler.m
@@ -5,11 +5,12 @@ function mp_descrambled = mp_descrambler(scrambled)
     mp_descrambled = zeros(1, length(scrambled));
     
     for i = 1 : length(scrambled)
-        register(1) = scrambled(i);
+        t = xor(register(18), register(23));
+        mp_descrambled(i) = xor(scrambled(i), t);
+        
         for j = 23 : -1 : 2
             register(j) = register(j-1);
         end
-        t = xor(register(18), register(23));
-        mp_descrambled(i) = xor(scrambled(i), t);
+        register(1) = scrambled(i);
     end    
 end
