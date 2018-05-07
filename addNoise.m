a5358ba0922b9c9b35ac16722c28129718ff2e01
RegRom <rego.romeo@gmail.com>
2018-05-07 23:27:42 +0200

    Poprawienie funkcji zaszumiającej

--- addNoise.m
+++ addNoise.m
@@ -1,11 +1,10 @@
-function distorted_signal = addNoise(rawSignal)
+function distorted_signal = addNoise(rawSignal, probability)
     %% Function simulates transmission through a channel by adding a noise
     
-    noise = randn(1, size(rawSignal, 2));
     distorted_signal = zeros(1, length(rawSignal));
     
     for i = 1 : length(rawSignal)
-        if(noise(i) > 1.6) || (noise(i) < -1.6)
+        if rand(1) < probability
             distorted_signal(i) = ~rawSignal(i);
         else
             distorted_signal(i) = rawSignal(i);
